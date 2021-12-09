4d6236e5f5ed4cfb8f83d0ccb9ab44b7325c8042
gitqh <quhang19881208@163.com>
2021-12-09 11:44:54 +0800

    feat: rename entry class

--- src/main/java/tw/cn/cap/gtb/todo/TodoApp.java
+++ src/main/java/tw/cn/cap/gtb/todo/TodoApp.java
@@ -1,11 +1,11 @@
 package tw.cn.cap.gtb.todo;
 
-public class App {
+public class TodoApp {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(new TodoApp().getGreeting());
     }
 }
