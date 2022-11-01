f149bd5d2d4ca59ecfe3f55fc72516a47093b71c
Jingwen Xia <amyfor7@gmail.com>
2022-11-01 14:35:40 +0800

    [Jingwen Xia] chore: delete greeting message

--- src/main/java/tw/cn/cap/gtb/todo/TodoApp.java
+++ src/main/java/tw/cn/cap/gtb/todo/TodoApp.java
@@ -1,11 +1,7 @@
 package tw.cn.cap.gtb.todo;
 
 public class TodoApp {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new TodoApp().getGreeting());
+
     }
 }
